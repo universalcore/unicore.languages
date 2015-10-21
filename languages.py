ce0acc71eb5fc5ab871eafff2f5f8df7ab9d65b9
codiebeulaine <codiebeulaine@gmail.com>
2015-10-21 13:04:34 +0200

    added available language files

---
+++ languages.py
@@ -0,0 +1,6 @@
+available_languages = [(u'eng_GB', u'English'), (u'hin_IN', u'Hindi'),
+                       (u'mar_IN', u'Marathi'), (u'tel_IN', u'Telugu'),
+                       (u'tam_IN', u'Tamil'),
+                       (u'ben_BD', u'Bengali (Bengali, Bangla)'),
+                       (u'asm_IN', u'Assamese'), (u'spa_ES', u'Spanish'),
+                       (u'swa_KE', u'Swahili')]
